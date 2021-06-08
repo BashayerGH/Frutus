c9b31a99cc82da40a0df3a20e6280ef45ffe46f8
Bashayr AlGhamdi <BashayerGh1@gmail.com>
2021-06-08 06:41:44 +0300

    Initial Commit

---
+++ Frutus/FrutusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrutusApp.swift
+//  Frutus
+//
+//  Created by Bashayr on 27/10/1442 AH.
+//
+
+import SwiftUI
+
+@main
+struct FrutusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
